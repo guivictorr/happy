621e172099c6ba513de4affc349a5aab1a0b4105
Guilherme <guilhermevictor20@gmail.com>
2020-10-12 15:00:02 -0300

    web: finalized landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-
+import { FiArrowRight } from 'react-icons/fi';
 import { Container } from './styles';
+
+import logo from '../../assets/images/logo.svg';
 
 const Landing: React.FC = () => {
   return (
     <Container>
-      <h1>Landing</h1>
+      <div>
+        <img src={logo} alt="Happy" />
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <section>
+          <strong>Natal</strong>
+          <span>Rio Grande do Norte</span>
+        </section>
+
+        <a href="/">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </a>
+      </div>
     </Container>
   );
 };
--- web/src/pages/Landing/styles.ts
+++ web/src/pages/Landing/styles.ts
@@ -1,3 +1,76 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import landingImage from '../../assets/images/landing.svg';
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  div {
+    position: relative;
+    width: 100%;
+    max-width: 1100px;
+    height: 100%;
+    max-height: 680px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    background: url(${landingImage}) no-repeat 80% center;
+  }
+
+  main {
+    max-width: 350px;
+
+    h1 {
+      font-size: 76px;
+      font-weight: 900;
+      line-height: 70px;
+    }
+
+    p {
+      margin-top: 40px;
+      font-size: 24px;
+      line-height: 34px;
+    }
+  }
+
+  section {
+    position: absolute;
+    right: 0;
+    top: 0;
+    font-size: 24px;
+    line-height: 34px;
+    display: flex;
+    flex-direction: column;
+
+    strong {
+      font-weight: 800;
+    }
+  }
+
+  a {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+
+    width: 80px;
+    height: 80px;
+    background: #ffd666;
+    border-radius: 30px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: background-color 0.2s ease;
+
+    &:hover {
+      background-color: #96feff;
+    }
+  }
+`;
