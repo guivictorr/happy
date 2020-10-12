f099b326f7252c7fc0f4785cc069d10495ac96d1
Guilherme <guilhermevictor20@gmail.com>
2020-10-12 14:22:47 -0300

    web: global styles and eslint configuration

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Teste</h1>
+    </>
+  );
+};
+
+export default App;
