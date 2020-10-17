754762915aaafb5fd09cf9adffab541b6e8eeaab
Guilherme <guilhermevictor20@gmail.com>
2020-10-17 01:00:44 -0300

    mobile: createorphanages page style

--- mobile/src/pages/CreateOrphanage/index.tsx
+++ mobile/src/pages/CreateOrphanage/index.tsx
@@ -1,14 +1,58 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { Switch } from 'react-native';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  Label,
+  Input,
+  ImagesInput,
+  SwitchContainer,
+  NextButton,
+  NextButtonText,
+} from './styles';
 
-const CreateOrphanage: React.FC = () => {
+const OrphanageData: React.FC = () => {
   return (
-    <Container>
-      <Text>CreateOrphanage</Text>
+    <Container contentContainerStyle={{ padding: 24 }}>
+      <Title>Dados</Title>
+
+      <Label>Nome</Label>
+      <Input />
+
+      <Label>Sobre</Label>
+      <Input style={{ height: 110, textAlignVertical: 'top' }} multiline />
+
+      <Label>Whatsapp</Label>
+      <Input />
+
+      <Label>Fotos</Label>
+      <ImagesInput>
+        <Feather name="plus" size={24} color="#15B6D6" />
+      </ImagesInput>
+
+      <Title>Visitação</Title>
+
+      <Label>Instruções</Label>
+      <Input style={{ height: 110, textAlignVertical: 'top' }} multiline />
+
+      <Label>Horario de visitas</Label>
+      <Input />
+
+      <SwitchContainer>
+        <Label>Atende final de semana?</Label>
+        <Switch
+          thumbColor="#fff"
+          trackColor={{ false: '#ccc', true: '#39CC83' }}
+        />
+      </SwitchContainer>
+
+      <NextButton>
+        <NextButtonText>Cadastrar</NextButtonText>
+      </NextButton>
     </Container>
   );
 };
 
-export default CreateOrphanage;
+export default OrphanageData;
--- mobile/src/pages/CreateOrphanage/styles.ts
+++ mobile/src/pages/CreateOrphanage/styles.ts
@@ -1,5 +1,61 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
+  flex: 1;
+`;
 
+export const Title = styled.Text`
+  color: #5c8599;
+  font-size: 24px;
+  font-family: 'Nunito_700Bold';
+  margin-bottom: 32px;
+  padding-bottom: 24px;
+`;
+
+export const Label = styled.Text`
+  color: #8fa7b3;
+  font-family: 'Nunito_600SemiBold';
+  margin-bottom: 8px;
+`;
+
+export const Input = styled.TextInput`
+  background-color: #fff;
+  border: 1.4px solid #d3e2e6;
+  border-radius: 20px;
+  height: 56px;
+  padding: 18px 24px;
+  margin-bottom: 16px;
+`;
+
+export const ImagesInput = styled.TouchableOpacity`
+  background-color: rgba(255, 255, 255, 0.5);
+  border: 1.4px dashed #96d2f0;
+  border-radius: 20px;
+  height: 56px;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 32px;
+`;
+
+export const SwitchContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 16px;
+`;
+
+export const NextButton = styled(RectButton)`
+  background-color: #15c3d6;
+  border-radius: 20px;
+  justify-content: center;
+  align-items: center;
+  height: 56px;
+  margin-top: 32px;
+`;
+
+export const NextButtonText = styled.Text`
+  font-family: 'Nunito_800ExtraBold';
+  font-size: 16px;
+  color: #fff;
 `;
